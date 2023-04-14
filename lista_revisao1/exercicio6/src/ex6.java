c17e46e55442f898ad53de5f93b9094bbd80d241
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2023-04-13 21:03:31 -0300

    Update ex6.java

--- lista_revisao1/exercicio6/src/ex6.java
+++ lista_revisao1/exercicio6/src/ex6.java
@@ -1,5 +1,38 @@
 public class ex6 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float marco1, marco2, tempo = 0, distancia = 0;
+        int i = 1, velocidade = 0;
+    
+        marco1 = input.nextFloat();
+        marco2 = input.nextFloat();
+    
+        while (marco1 != marco2) {
+    
+            System.out.printf("MARCO 1 = %.2f\n", marco1);
+            System.out.printf("MARCO 2 = %.2f\n", marco2);
+    
+            i++;
+            velocidade = i * 10;
+            distancia = marco2 - marco1;
+            tempo = distancia / velocidade;
+    
+            while (tempo >= 2 && i <= 8) {
+                System.out.printf("VELOCIDADE = %dKm/h DECORREU O TEMPO DE %.1f HORAS\n", velocidade, tempo);
+    
+                i++;
+                velocidade = i * 10;
+                distancia = marco2 - marco1;
+                tempo = distancia / velocidade;
+            }
+    
+            i = 1;
+            marco1 = input.nextFloat();
+            marco2 = input.nextFloat();
+    
+        }
+    
+        input.close();
     }
 }
