e1cf6b0198ce954afef6d1298e9d01c3b62a1655
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2023-04-13 21:02:57 -0300

    Update ex5.java

--- lista_revisao1/exercicio5/src/ex5.java
+++ lista_revisao1/exercicio5/src/ex5.java
@@ -1,5 +1,19 @@
 public class ex5 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   public static void main(String[] args) throws Exception {
+        
+        Double A = 90000.0, B = 200000.0;
+        int qntAno = 0;
+
+        do {
+
+            A += A * 0.03;
+            B += B * 0.015;
+
+            qntAno++;
+            
+        } while (A < B);
+
+        System.out.println("Em "+ qntAno +" anos a cidade A ultrapasará ou se igualará a quantidade de abitantes da cidade B.");
+
     }
 }
