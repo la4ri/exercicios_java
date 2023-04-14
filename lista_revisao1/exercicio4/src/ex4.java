0c27c76d306eb9900fc23929dfea1ed2ea56ab5f
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2023-04-13 21:02:09 -0300

    Update ex4.java

--- lista_revisao1/exercicio4/src/ex4.java
+++ lista_revisao1/exercicio4/src/ex4.java
@@ -1,5 +1,30 @@
 public class ex4 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner input = new Scanner(System.in);
+        int qntTermo;
+        Double soma = 100.0, dividendo = 997.0, divisor = 2.0;
+
+
+        System.out.print("Escreva a quantidade de termos a ser calculado: ");
+        qntTermo = input.nextInt();
+
+        for (int i = 1; i < qntTermo; i++) {
+
+            if (divisor % 2 == 0) {
+                soma += - (dividendo/divisor);
+            } else {
+                soma += (dividendo/divisor);
+            }
+
+            dividendo += -3;
+            divisor += 1;
+
+        }
+
+        System.out.print("Soma: " + soma);
+
+        input.close();
+
     }
 }
